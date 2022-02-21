cc09fb8e6dcb9fb133c9e15c0166d654f627dd92
wbhaao <a31620504@gmail.com>
2022-02-21 16:24:01 +0900

    fourth commit

---
+++ web.1/main.js
@@ -0,0 +1,15 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+canvas.width  = window.innerWidth - 100;
+canvas.height  = window.innerHeight - 100;
+
+var dino = {
+    x : 10,
+    y : 200,
+    width : 50,
+    height : 50,
+    draw(){
+        ctx.fillStyle
+    }
+}
